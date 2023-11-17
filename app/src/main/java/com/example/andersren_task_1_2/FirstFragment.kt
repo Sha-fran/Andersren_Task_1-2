e7d8238c813598a55ed5665e77d2f8144ec5ae13
foxstudent103280 <dCu88WVcucnA>
2023-11-17 11:28:31 +0200

    Add toast message

--- app/src/main/java/com/example/andersren_task_1_2/FirstFragment.kt
+++ app/src/main/java/com/example/andersren_task_1_2/FirstFragment.kt
@@ -1,22 +1,40 @@
 package com.example.andersren_task_1_2
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.andersren_task_1_2.databinding.FirstFragmentBinding
 
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), TextWatcher {
+    private var _binding:FirstFragmentBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.first_fragment, container, false)
+    ): View {
+        _binding = FirstFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.editText.addTextChangedListener(this)
     }
 
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        Toast.makeText(requireContext(), binding.editText.text.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+    }
 }
